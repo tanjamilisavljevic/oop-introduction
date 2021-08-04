6f1df5838b9feb02aeae548adeae23d3f586049f
Tanja <tanjamilisavljevic7@gmail.com>
2021-08-04 11:45:00 +0200

    Create class Beer and give it properties

--- index.php
+++ index.php
@@ -6,18 +6,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require 'drink.php';
+require 'beer.php';
+
 
 $cola = new Drink('black', 2);
 $cola -> getInfo();
 
-/* EXERCISE 1
-Create a class beverage.
-Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
-Have a default value "cold" in the construct for temperature.
-Remember for now we will use properties and methods that can be accessed from everywhere.
-Make a getInfo function which returns "This beverage is <temperature> and <color>."
-Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
- print the getInfo on the screen.
-print the temperature on the screen.
-USE TYPEHINTING EVERYWHERE!
-*/
+$whatebever = new Beer('isbeer', 2);
+$whatebever -> getInfo();
